cc3ef464826a26f49abec0e3633dc173af4ed06a
Trey Griffith <trey.griffith@gmail.com>
2020-07-14 10:24:21 -0700

    types for authorize

--- lib/connect.ts
+++ lib/connect.ts
@@ -1,27 +1,27 @@
-import { IKitConfig } from './config'
+import { AuthorizedConfig } from './config'
 import { Connector } from './api/connector'
 import { createConnection, getConnection, Connection } from './api/connection'
 import { createAuthorization } from './api/authorization'
 import { authorize, AuthWindow } from './authorize'
 
-async function updateConnection(config: IKitConfig, connection: Connection): Promise<Connection> {
+async function updateConnection(config: AuthorizedConfig, connection: Connection): Promise<Connection> {
   const newConnection = await getConnection(config, connection.connector.slug)
   return newConnection
 }
 
 // return an error?
 // handle not logged in - send back to their site
-export async function connect (config: IKitConfig, authWindow: AuthWindow, connector: Connector): Promise<Connection> {
+export async function connect (config: AuthorizedConfig, authWindow: AuthWindow, connector: Connector): Promise<Connection> {
   const connection = await createConnection(config, connector.slug)
   const authorization = await authorize(config, authWindow, connection.authorization)
   const newConnection = await updateConnection(config, connection)
   return newConnection
 }
 
-export async function reconnect (config: IKitConfig, authWindow: AuthWindow, connection: Connection): Promise<Connection> {
+export async function reconnect (config: AuthorizedConfig, authWindow: AuthWindow, connection: Connection): Promise<Connection> {
   const oldAuthorization = connection.authorization
   if (!oldAuthorization) {
-    const newConnection = await connect(config, connection.connector)
+    const newConnection = await connect(config, authWindow, connection.connector)
     return newConnection
   }
 
