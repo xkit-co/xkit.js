0f9e82f7b1b983d67698add698e65ce626a81cbd
Rowland I. Ekemezie <15085641+rowlandekemezie@users.noreply.github.com>
2021-06-10 23:49:57 +0800

    Update assets/xkit-js/src/api/socket.ts

--- src/api/socket.ts
+++ src/api/socket.ts
@@ -83,8 +83,8 @@ async function promisifyPush (push: Push): Promise<unknown> {
       .receive('ok', (response) => {
         resolve(response)
       })
-      .receive('error', ({ reason }: { reason: string}) => {
-        logger.debug(`Received error response: ${reason}`)
+      .receive('error', ({ reason }) => {
+        logger.debug(`Received error response: ${String(reason)}`)
         reject(new Error(reason))
       })
       .receive('timeout', () => {
